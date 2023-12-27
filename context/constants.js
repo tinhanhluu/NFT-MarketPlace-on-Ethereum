095a4aae2e569d378d3b2e5a99a99b9db98d7dc0
Lưu Tinh Anh <84430093+tinhanhluu@users.noreply.github.com>
2023-12-27 23:22:30 +0700

    context

---
+++ context/constants.js
@@ -0,0 +1,8 @@
+import realEstate from "./RealEstate.json";
+
+//REAL ESTATE
+//LOCAL ADDRESS
+export const REAL_ESTATE_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+//POLYGON ADDRESS
+// export const REAL_ESTATE_ADDRESS = "0x369ec6539Df1C60a9A0Eb00C413ED851f8d6cd4F";
+export const REAL_ESTATE_ABI = realEstate.abi;
